1c067c96444cc6ebea71c1344bf41ae06f2e9c53
Bolek Kurowski <bolek@alumni.cmu.edu>
2017-08-08 00:53:35 -0400

    Update ruby aliases

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,3 +1,4 @@
-alias sc='script/console'
-alias sg='script/generate'
-alias sd='script/destroy'
+alias sc='bin/console'
+alias sg='bin/generate'
+alias sd='bin/destroy'
+alias rake='noglob bundle exec rake'
